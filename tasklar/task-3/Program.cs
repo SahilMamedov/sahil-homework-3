cf4a5ec71040372ef40e6eda45e4782ded82844f
SahilMamedov <sahilim@code.edu.az>
2022-03-14 17:44:50 +0400

    rafakto: kodlarin yerinde deyishiklik etdim.

--- tasklar/task-3/Program.cs
+++ tasklar/task-3/Program.cs
@@ -8,51 +8,50 @@ namespace task_3
         {
             // 3) Bir method olsun göndərilən "n" sayda ədədin hamsını toplayıb geri qaytarsın.
 
-            Console.WriteLine("Nece eded toplanshin ?");
-            int array = Convert.ToInt32(Console.ReadLine());
+            // commentde olan kodlar 2-ci usula aitdir 
 
-            int[] eded = new int[array];
+            //Console.WriteLine("Nece eded toplanshin ?");
+            //int array = Convert.ToInt32(Console.ReadLine());
 
-            toplama(eded);
+            //int[] eded = new int[array];
 
-          // 2ci hell usulu  toplama(6, 7, 8);
+            //toplama(eded);
 
-            //    Console.WriteLine(toplama(6, 7, 8));
+            toplama(6, 7, 8);
+
+            Console.WriteLine(toplama(6, 7, 8));
         }
-
-        static int toplama(int[] eded)
+        static int toplama(int result, params int[] eded)
         {
-            
-            int result = 0;
+
             for (int i = 0; i < eded.Length; i++)
             {
-                Console.Write((i + 1).ToString() + ".Ededi daxil edin: ");
-                eded[i] = Convert.ToInt32(Console.ReadLine());
+
                 result += eded[i];
 
             }
-            Console.WriteLine($"sum-----{result}");
+            Console.WriteLine(result);
             return result;
 
-
-            //2 - ci hell usulu
-
-
-            //}
-
-
-            //static int toplama(int result, params int[] eded)
-            //{
-
-            //    for (int i = 0; i < eded.Length; i++)
-            //    {
-
-            //        result += eded[i];
-
-            //    }
-
-            //    return result;
-
         }
+
+        //static int toplama(int[] eded)
+        //{
+
+        //    int result = 0;
+        //    for (int i = 0; i < eded.Length; i++)
+        //    {
+        //        Console.Write((i + 1).ToString() + ".Ededi daxil edin: ");
+        //        eded[i] = Convert.ToInt32(Console.ReadLine());
+        //        result += eded[i];
+
+        //    }
+        //    Console.WriteLine($"sum-----{result}");
+        //    return result;
+
+
+        //}
+ 
+        
     }
 }
